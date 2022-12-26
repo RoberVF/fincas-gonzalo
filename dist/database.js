eb8fda875ae2064a9003bbf2ed7d76c84f31ca3e
Roberto Morales Laptop <robertomoralesfumero@gmail.com>
2022-12-26 18:24:13 +0000

    Graficas

--- dist/database.js
+++ dist/database.js
@@ -3,7 +3,7 @@ var __createBinding = (this && this.__createBinding) || (Object.create ? (functi
     if (k2 === undefined) k2 = k;
     var desc = Object.getOwnPropertyDescriptor(m, k);
     if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
+        desc = { enumerable: true, get: function() { return m[k]; } };
     }
     Object.defineProperty(o, k2, desc);
 }) : (function(o, m, k, k2) {
@@ -15,10 +15,12 @@ var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (
 }) : function(o, v) {
     o["default"] = v;
 });
-var __importStar = (this && this.__importStar) || function (mod) {
+var __importStar = (this && this.__importStar) || function(mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    if (mod != null)
+        for (var k in mod)
+            if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
     __setModuleDefault(result, mod);
     return result;
 };
@@ -36,8 +38,7 @@ async function startConnection() {
             useFindAndModify: true
         });
         console.log("Database is connected");
-    }
-    catch (e) {
+    } catch (e) {
         console.log(e);
     }
 }
--- src/database.ts
+++ src/database.ts
@@ -3,7 +3,6 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 const MONGODB_URI= `mongodb+srv://${process.env.NAME}:${process.env.PASS}@${process.env.DATABASE}`
-
 export async function startConnection(){
     try{
         await connect(MONGODB_URI,{
